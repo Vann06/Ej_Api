d05835cf1c212b48be98aa13e2f49417b2adde4b
Vann06 <cas23201@uvg.edu.gt>
2025-05-15 20:46:07 -0600

    mini fix

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -9,14 +9,26 @@ import (
 func main() {
 	r := gin.Default()
 
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+
+	// Archivos estáticos
 	r.Static("/frontend", "./frontend")
 
 	// Conexión a la base de datos
 	connection := db.ConnectDB()
 
-	// Registrar las rutas y pasar la conexión
+	// Registrar rutas
 	routes.RegisterIncidentRoutes(r, connection)
 
-	// Levantar el servidor
+	// Levantar servidor
 	r.Run(":8080")
 }
--- db/db.go
+++ db/db.go
@@ -4,11 +4,12 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func ConnectDB() *sql.DB {
-	// Colocar aqui direccion de tu base de datos 
+	// Colocar aqui direccion de tu base de datos
 	dsn := "Vann:Chapi2019@tcp(127.0.0.1:3306)/incidentes"
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
@@ -19,7 +20,7 @@ func ConnectDB() *sql.DB {
 	if err != nil {
 		log.Fatal("No se pudo conectar a la base de datos:", err)
 	}
-	
+
 	fmt.Println("✅ Conectado a la base de datos MySQL")
 	return db
 }
